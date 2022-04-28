57638d4a8b8d3c1a25327f8cdacea13517292078
Vladimir <summeroff@gmail.com>
2022-04-28 14:01:19 -0700

    remove commented lines

--- cmake/Modules/ObsCpack.cmake
+++ cmake/Modules/ObsCpack.cmake
@@ -54,8 +54,6 @@ endif()
 
 set(CPACK_BUNDLE_NAME "OBS")
 set(CPACK_BUNDLE_PLIST "${CMAKE_SOURCE_DIR}/mac/vcam-plugin/Info.plist")
-#set(CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/cmake/osxbundle/obs.icns")
-#set(CPACK_BUNDLE_STARTUP_COMMAND "${CMAKE_SOURCE_DIR}/cmake/osxbundle/obslaunch.sh")
 
 set(CPACK_WIX_TEMPLATE "${CMAKE_SOURCE_DIR}/windows/obs-virtual-cam/cmake/Modules/WIX.template.in")
 
